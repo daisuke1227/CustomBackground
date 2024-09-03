2ff7b6c5351c9db443185fb0824577d215540f1f
camila314 <47485054+camila314@users.noreply.github.com>
2024-09-03 17:40:32 -0500

    update for geode 3.4.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,66 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CCSprite.hpp>
 #include <Geode/modify/LevelSearchLayer.hpp>
+#include <Geode/modify/CCScale9Sprite.hpp>
 
 using namespace geode::prelude;
 using cocos2d::extension::CCScale9Sprite;
 
 #define $ $modify
 
-class $(LevelSearchLayer) {
+class $modify(CCScale9Sprite) {
+	struct Fields {
+		bool m_hasFixed = false;
+	};
+
+	void visit() {
+		CCScale9Sprite::visit();
+		if (!m_fields->m_hasFixed) {
+			fixSprites();
+			m_fields->m_hasFixed = true;
+		}
+	}
+
+	void transformSprite(CCSprite* sprite, CCSize transform, int idx) {
+		auto tRect = sprite->getTextureRect();
+
+		tRect.size -= transform;
+
+		if (idx == 0)
+			sprite->setPosition(sprite->getPosition() + transform);
+		else if (idx == 1 && (transform.width > 0 || transform.height > 0))
+			sprite->setVisible(false);
+		else if (idx == 2)
+			tRect.origin += transform;
+
+		sprite->setTextureRect(tRect);
+	}
+
+	void fixSprites() {
+		CCSprite* sprites[3][3] = {
+			{_topLeft, _top, _topRight},
+			{_left, _centre, _right},
+			{_bottomLeft, _bottom, _bottomRight}
+		};
+
+		auto halfSize = getContentSize() / 2;
+
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				auto sprite = sprites[i][j];
+
+				auto tRect = sprite->getTextureRect();
+				auto diff = tRect.size - halfSize;
+				diff = CCSize(fmax(0, diff.width), fmax(0, diff.height));
+
+				transformSprite(sprite, CCSize(diff.width, 0), j);
+				transformSprite(sprite, CCSize(0, diff.height), i);
+			}
+		}
+	}
+};
+
+class $modify(LevelSearchLayer) {
 	bool init(int i) {
 		LevelSearchLayer::init(i);
 		auto mod = Mod::get();
@@ -36,7 +89,7 @@ class $(LevelSearchLayer) {
 	}
 };
 
-class $(CCSprite) {
+class $modify(CCSprite) {
 	static CCSprite* create(char const* name) {
 		auto sprite = CCSprite::create(name);
 
@@ -45,7 +98,7 @@ class $(CCSprite) {
 			auto col = mod->getSettingValue<ccColor3B>("color");
 
 			if (mod->getSettingValue<bool>("use_image")) {
-				auto img = mod->getSettingValue<ghc::filesystem::path>("image");
+				auto img = mod->getSettingValue<std::filesystem::path>("image");
 
 				auto spr2 = CCSprite::create(img.string().c_str());
 				if (spr2)
